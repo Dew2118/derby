f2331f8495925e8e7db00ef758a3b1c44171acdd
Dew2118 <50415635+Dew2118@users.noreply.github.com>
2020-11-21 07:40:33 +0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,6 +1,17 @@
 name = prompt('Please enter your name ');
 document.getElementById('output').innerHTML=name;
+
 var time = new Date();
 var dateandtime = time.toLocaleString();
 document.getElementById('a').innerHTML = dateandtime;
 console.log(name+dateandtime);
+
+function myFunction() {
+    console.log(document.getElementById('d1').value);
+}
+function no() {
+	console.log("no");}
+function yes() {
+	console.log("yes");}
+
+
